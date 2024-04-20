c906b3ce756e64666429a222aed2e6b43ff21bcb
nickhealthy <alskadmlcraz1@gmail.com>
2024-04-21 00:42:52 +0900

    rename project main class

--- src/main/java/io/spring/batch/helloworld/HelloWorldApplication.java
+++ src/main/java/io/spring/batch/helloworld/HelloWorldApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBatchApplication {
+public class HelloWorldApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBatchApplication.class, args);
+        SpringApplication.run(HelloWorldApplication.class, args);
     }
 
 }
