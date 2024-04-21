ee5f3222816508e1ea9f372187bddf254798f4ae
nickhealthy <alskadmlcraz1@gmail.com>
2024-04-21 12:58:20 +0900

    spring batch 기본 빈 확인 TEST 코드 작성

--- src/test/java/io/spring/batch/helloworld/HelloWorldApplicationTests.java
+++ src/test/java/io/spring/batch/helloworld/HelloWorldApplicationTests.java
@@ -2,9 +2,22 @@ package io.spring.batch.helloworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class HelloWorldApplicationTests {
+
+    @Test
+    void applicationContext() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext();
+
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println("beanDefinitionName = " + beanDefinitionName);
+        }
+    }
+
 
     @Test
     void contextLoads() {
