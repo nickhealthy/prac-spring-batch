80053ebc098d909140f8c8fc9826cf6c04902f2e
nickhealthy <alskadmlcraz1@gmail.com>
2024-06-06 18:36:23 +0900

    섹션2 - Hello Spring Batch 시작하기

--- src/main/java/io/spring/batch/helloworld/HelloWorldApplication.java
+++ src/main/java/io/spring/batch/helloworld/HelloWorldApplication.java
@@ -3,12 +3,9 @@ package io.spring.batch.helloworld;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -31,33 +28,43 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class HelloWorldApplication {
 
-    @Autowired
+    @Autowired // JobBuilder 인스턴스 생성
     private JobBuilderFactory jobBuilderFactory;
 
-    @Autowired
+    @Autowired // StepBuilder 인스턴스 생성
     private StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Step step() {
-        return this.stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                            @Override
-                            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                                System.out.println("Hello World!");
-                                return RepeatStatus.FINISHED;
-                    }
-                }).build();
-    }
-
-    @Bean
-    public Job job() {
-        return this.jobBuilderFactory.get("job")
-                .start(step())
-                .build();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    /**
+     *
+     * - this.stepBuilderFactory.get("step1"): stepBuilderFactory를 사용해 스텝 구성 및 StepBuilder 반환
+     *   -  StepBuilder: 해당 빌더를 사용해 스텝을 정의할 수 있다.
+     * - tasklet: 테스크릿을 사용해 스텝을 구성
+     * - build(): 스텝 구성 완료
+     */
+    @Bean
+    public Step step1() {
+        return this.stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Hello, World!");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    /**
+     * 스프링 배치 잡을 반환한다.
+     * - this.jobBuilderFactory.get("basicJob"): 잡 이름을 전달해 JobBuilder 얻기
+     * - start(step1()): 시작할 스텝을 지정
+     * - build(): 실제 잡을 생성
+     */
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("basicJob")
+                .start(step1())
+                .build();
     }
 
 }
