1b2f0d0310bb3703d73853c20a96866d5b6d8d26
nickhealthy <alskadmlcraz1@gmail.com>
2024-06-28 18:04:53 +0900

    섹션3 - JobExecution

--- src/main/java/io/spring/batch/helloworld/ch3/jobExecution/JobExecutionConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/ch3/jobExecution/JobExecutionConfiguration.java
@@ -1,2 +1,55 @@
-package io.spring.batch.helloworld.ch3.jobExecution;public class JobExecutionConfiguration {
+package io.spring.batch.helloworld.ch3.jobExecution;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobExecutionConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("JobParameterConfiguration")
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("STEP1 HAS EXECUTED!!");
+                    return RepeatStatus.FINISHED;
+                }).build();
+
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext)
+                            throws Exception {
+                        //throw new RuntimeException("강제 에러 발생!");
+                        System.out.println("STEP2 HAS EXECUTED!!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+
+    }
+
 }
