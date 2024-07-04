52fc736a5ff327338f69c5ec4ff8774f3bb412e4
nickhealthy <alskadmlcraz1@gmail.com>
2024-07-04 21:11:21 +0900

    섹션4 - validator

---
+++ src/main/java/io/spring/batch/helloworld/ch4/validator/ValidatorConfiguration.java
@@ -0,0 +1,56 @@
+package io.spring.batch.helloworld.ch4.validator;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class ValidatorConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("ValidatorConfiguration")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .validator(new CustomJobParametersValidator())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("STEP1 HAS EXECUTED!");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("STEP2 HAS EXECUTED!");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("STEP3 HAS EXECUTED!");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
