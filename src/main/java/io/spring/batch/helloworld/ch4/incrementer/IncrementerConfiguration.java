b3b7cfc4cdfc1485d24618079f0823f159c3bd2c
nickhealthy <alskadmlcraz1@gmail.com>
2024-07-16 21:37:23 +0900

    섹션4 - JobParametersIncrementer

--- src/main/java/io/spring/batch/helloworld/ch4/incrementer/IncrementerConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/ch4/incrementer/IncrementerConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +22,8 @@ public class IncrementerConfiguration {
         return jobBuilderFactory.get("IncrementerConfiguration")
                 .start(step1())
                 .next(step2())
-                .incrementer(new CustomJobParametersIncrementer())
+//                .incrementer(new CustomJobParametersIncrementer())    // 커스터마이징 JobParametersIncrementer
+                .incrementer(new RunIdIncrementer())                    // 스프링 배치에서 기본으로 제공해주는 JobParametersIncrementer
                 .build();
     }
 
